1664aba247e41a16e2b9a611a6ed05eb87efe934
Jewell <49321194+Jewe11@users.noreply.github.com>
2023-07-18 17:43:30 +0000

    init push
    source

---
+++ FetchCsvs.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UVACanvasAccess.ApiParts;
+
+namespace FetchReportsForCron
+{
+    public class FetchCsvs
+    {
+         public static async Task GetCsvs()
+        {
+            var token = Program.Config("token").ToString().Trim();
+            var rootDirectory = new DirectoryInfo(Program.Config("rootPath").ToString().Trim());
+            //Console.WriteLine(token);
+
+            var api = new Api(token,
+                Program.Config("baseURL").ToString());
+            var termNameFilter = Program.Config("termFilter").ToString() == string.Empty ? null : Program.Config("termFilter").ToString().Trim().Split(',');
+
+            var csvNameFilter =  Program.Config("csvFilter").ToString() == string.Empty ? null : Program.Config("csvFilter").ToString().Trim().Split(',');
+
+            var currTermsGp = api.StreamEnrollmentTerms()
+                .Where(ter => ter.EndAt > DateTime.Today && ter.GradingPeriodGroupId is not null);
+            var currTermsNoGp = api.StreamEnrollmentTerms()
+                .Where(ngp => ngp.EndAt > DateTime.Today && ngp.GradingPeriodGroupId is null);
+
+            if (currTermsGp != null)
+            {
+                await foreach (var term in currTermsGp)
+                {
+                    if (termNameFilter != null && termNameFilter.Any(word => Regex.IsMatch(term.Name, @$"\b{word}\b", RegexOptions.IgnoreCase)))
+                        continue;
+                    Console.WriteLine(term.ToPrettyString());
+                    await Task.Run(async () =>
+                    {
+                        var r = await api.StartReport("mgp_grade_export_csv",
+                            new[]
+                            {
+                                ("enrollment_term_id", (object) term.Id)
+                            });
+                        while ("complete" != r.Status)
+                        {
+                            await Task.Delay(1000 * 30);
+                            r = await r.Refresh();
+                        }
+
+                        var data = await r.Attachment.Download();
+                        //file name
+                        var path =
+                            rootDirectory + "\\mgp.zip";
+                        if (data != null) 
+                            File.WriteAllBytes(path,
+                                data);
+                        Console.WriteLine(term.Id); 
+                        Unzip(term.Id);
+                        //Console.WriteLine("Run Unzip Method");
+                    });
+                }
+            }
+            if (currTermsNoGp != null)
+            {
+                await foreach (var term in currTermsNoGp)
+                {
+                    if (csvNameFilter != null && !csvNameFilter.Any(word =>
+                            Regex.IsMatch(term.Name, @$"\b{word}\b", RegexOptions.IgnoreCase)))
+                    if (termNameFilter != null && termNameFilter.Any(word => 
+                            Regex.IsMatch(term.Name, @$"\b{word}\b", RegexOptions.IgnoreCase))) 
+                        continue;
+                    
+                    Console.WriteLine(term.ToPrettyString());
+                    
+                    await Task.Run(async () =>
+                    {
+                        var r = await api.StartReport("grade_export_csv",
+                            new[]
+                            {
+                                ("enrollment_term_id", (object) term.Id)
+                            });
+                        while ("complete" != r.Status)
+                        {
+                            await Task.Delay(1000 * 30);
+                            r = await r.Refresh();
+                        }
+
+                        var data = await r.Attachment.Download();
+                        //file name
+                        var path = rootDirectory + "\\" + term.Id + ".csv";
+                        if (data != null)
+                            File.WriteAllBytes(path,
+                                data);
+                        //Console.WriteLine();
+                        Console.WriteLine(term.Id + " -wrote csv");
+                    });
+                }
+            }
+        }
+
+        private static void Unzip(ulong termId)
+        {
+            var terId = termId.ToString();
+            var zipPath = Program.Config("rootPath") + "\\mgp.zip";
+            var extractPath = Program.Config("rootPath").ToString();
+            if (new FileInfo(zipPath).Length == 0)
+            {
+                // empty
+                File.Delete(zipPath);
+            }
+            else
+            {
+                var zip = ZipFile.OpenRead(zipPath);
+                foreach (var entry in zip.Entries)
+                {
+                    var destinationPath = Path.GetFullPath(Path.Combine(extractPath, terId + ".csv"));
+                    entry.ExtractToFile(destinationPath, true);
+                    zip.Dispose();
+                }
+            }
+
+            //Console.WriteLine("Extracted");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Nett;
+
+namespace FetchReportsForCron
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            var rootDirectory = new DirectoryInfo(Program.Config("rootPath").ToString().Trim());
+            Console.WriteLine(rootDirectory.Exists);
+            if (Config("purgeFilesBefore").ToString().Trim() == "true")
+            {
+                if (rootDirectory.Exists)
+                {
+                    await Empty(rootDirectory, "*csv");
+                }
+                
+            }
+            //-------
+            await FetchCsvs.GetCsvs();
+            //-------
+            if (Config("purgeFilesAfter").ToString().Trim() == "true")
+            {
+                await Empty(rootDirectory, "*csv");
+            }
+        }
+        public static TomlObject Config(string keyV)
+        {
+            //var workingDirectory =System.Reflection.Assembly.GetExecutingAssembly().Location;
+            //var parentDirectory =  Directory.GetParent(workingDirectory)?.Parent?.FullName;
+            TomlObject info = null;
+            //Console.WriteLine(parentDirectory+"\\config.toml");
+            {
+                var exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                var exeDir = Path.GetDirectoryName(exePath);
+                //var binDir = System.IO.Directory.GetParent(exeDir ?? throw new InvalidOperationException());
+                var parseToml = Toml.ReadFile(exeDir + "\\config.toml");
+                var data = parseToml.Rows.AsQueryable().Where(x => x.Key == keyV);
+                foreach (var keyValuePair in data)
+                {
+                    info = keyValuePair.Value;
+                }
+            }
+            return info;
+        }
+        private static async Task Empty(DirectoryInfo directory, string ext)
+        {
+            await Task.Delay(1000);
+            foreach(var file in directory.GetFiles($"{ext}")) file.Delete();
+            Console.WriteLine($"Dumped {directory.Name}");
+        }
+    }
+}
